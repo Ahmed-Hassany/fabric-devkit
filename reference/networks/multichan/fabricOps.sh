9bffd062d22ec98e5db5b98b61aa12c30c2af0c4
Paul Sitoh <paulwizviz@gmail.com>
2019-01-16 20:49:04 +0000

    Remove multi-channel network reference
    Multi-channel is now accomodated in solo reference network

--- reference/networks/multichan/fabricOps.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose -f ./docker-compose.yml run --rm assets.generator /bin/bash -c '${PWD}/generate-artifacts.sh'
--- reference/networks/multichan/networkOps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker-compose up -d orderer.example.com
-docker-compose up -d couchdb0
-docker-compose up -d couchdb1
-docker-compose up -d peer0.org1.example.com
-docker-compose up -d peer0.org2.example.com
-docker-compose up -d cli
