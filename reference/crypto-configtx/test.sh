1abd22708925d30c80c9da5560b2f4b20199bc24
Paul Sitoh <paulwizviz@gmail.com>
2019-01-17 15:34:22 +0000

    Fix crypto-configtx reference
    Change to using docker command instead of docker-compose.

--- reference/crypto-configtx/test.sh
+++ reference/crypto-configtx/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose run --rm fabric-tools /bin/bash -c '${PWD}/generate-artefacts.sh'
+docker run --rm -e "GOPATH=/opt/gopath" -e "FABRIC_CFG_PATH=/opt/gopath/src/github.com/hyperledger/fabric" -w="/opt/gopath/src/github.com/hyperledger/fabric" --volume=${PWD}:/opt/gopath/src/github.com/hyperledger/fabric hyperledger/fabric-tools /bin/bash -c '${PWD}/generate-artefacts.sh'
