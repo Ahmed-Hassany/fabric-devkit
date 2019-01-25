9c76ebf785d40bf1b27b11342ed7f372c19c7538
Paul Sitoh <paulwizviz@gmail.com>
2019-01-25 15:41:00 +0000

    Fix reference crypto-configtx lab
    * Removed the superfluous .env
    * Ensure that the test scipt reset the lab to clean state when started.

--- reference/crypto-configtx/test.sh
+++ reference/crypto-configtx/test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker run --rm -e "GOPATH=/opt/gopath" -e "FABRIC_CFG_PATH=/opt/gopath/src/github.com/hyperledger/fabric" -w="/opt/gopath/src/github.com/hyperledger/fabric" --volume=${PWD}:/opt/gopath/src/github.com/hyperledger/fabric hyperledger/fabric-tools /bin/bash -c '${PWD}/generate-artefacts.sh'
+FABRIC_TOOL_VERSION=latest
+
+rm -rf ./assets
+
+docker run --rm -e "GOPATH=/opt/gopath" \
+                -e "FABRIC_CFG_PATH=/opt/gopath/src/github.com/hyperledger/fabric" \
+                -w="/opt/gopath/src/github.com/hyperledger/fabric" \
+                --volume=${PWD}:/opt/gopath/src/github.com/hyperledger/fabric \
+                hyperledger/fabric-tools:$FABRIC_TOOL_VERSION /bin/bash -c '${PWD}/generate-artefacts.sh'
