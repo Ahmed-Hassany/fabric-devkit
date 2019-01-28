2f7080d7541485625f7ea8477e3a6a274da102bb
Paul Sitoh <paulwizviz@gmail.com>
2019-01-28 19:21:23 +0000

    Fix reference network dev
    Update anchor peers

--- reference/networks/dev/cli-scripts/channelOps.sh
+++ reference/networks/dev/cli-scripts/channelOps.sh
@@ -3,5 +3,6 @@
 . ./scripts/common.sh
 
 peer channel create -o $ORDERER -c $CHANNEL_NAME -f ./channel-artefacts/$CHANNEL_NAME.tx --tls --cafile $ORDERER_CA
+peer channel update -f ./channel-artefacts/Org1MSPanchors_$CHANNEL_NAME.tx -c $CHANNEL_NAME -o $ORDERER --tls --cafile $ORDERER_CA
 peer channel join -o $ORDERER -b ./$CHANNEL_NAME.block --tls --cafile $ORDERER_CA
 
